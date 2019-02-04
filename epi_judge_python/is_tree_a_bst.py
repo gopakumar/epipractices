dc5ef3ff7004cdc1eb181ddcb549a18e1d2a6034
GKS <gopakumar.in@gmail.com>
2019-02-04 03:09:57 -0800

    Bst validation 414 pass

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -1,9 +1,51 @@
 from test_framework import generic_test
 
+#flag = True
 
 def is_binary_tree_bst(tree, low_range=float('-inf'), high_range=float('inf')):
     # TODO - you fill in here.
-    return True
+    global flag
+    flag = True
+
+    def helper(tree,is_left):
+        global flag
+        if (tree and flag):
+            if ((tree.left) and (tree.left.data > tree.data)):
+                flag = False
+                return None
+            if ((tree.right) and (tree.right.data < tree.data)):
+                flag = False
+                return None
+            leftmax = helper(tree.left,True)
+
+            rightmin = helper(tree.right,False)
+
+            if (leftmax is not None) and (leftmax > tree.data):
+                flag = False
+                return None
+            if (rightmin is not None) and (rightmin < tree.data):
+                flag = False
+                return None
+
+            leftmax = leftmax if leftmax is not None  else tree.data
+            rightmin = rightmin if rightmin is not None  else tree.data
+            print ("max , min ",leftmax,rightmin,tree.data)
+
+            if (is_left is None):
+                if (leftmax > tree.data or rightmin < tree.data):
+                    flag = False
+                    return 
+            
+            if (is_left is True ):
+                return max(leftmax,rightmin,tree.data)
+            else:
+                return min(leftmax,rightmin,tree.data)
+        else:
+            return None
+
+    helper(tree, None)
+    return flag
+
 
 
 if __name__ == '__main__':
