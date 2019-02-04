f1c1423a78f4af80eb2082253c81eae5bd2080e9
GKS <gopakumar.in@gmail.com>
2019-02-03 16:45:15 -0800

    anagram compelete

--- epi_judge_python/anagrams.py
+++ epi_judge_python/anagrams.py
@@ -3,7 +3,28 @@ from test_framework import generic_test, test_utils
 
 def find_anagrams(dictionary):
     # TODO - you fill in here.
-    return []
+    list_strings = dictionary
+    #print (list_strings)
+    result = list()
+    data_dict = dict()
+    for  i,str in enumerate(list_strings):
+        tkey = "".join(sorted(str))
+        if tkey in data_dict:
+            data_dict[tkey].append(list_strings[i])
+        else:
+            data_dict[tkey] = list()
+            data_dict[tkey].append(list_strings[i])
+    
+    #print(data_dict)
+    for k,v in data_dict.items():
+        if len(data_dict[k]) >1:
+            result.append(data_dict[k])
+
+#    for x in result:
+#        print (x)
+            
+
+    return result
 
 
 if __name__ == '__main__':
