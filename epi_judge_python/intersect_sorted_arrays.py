a8f9c146dcd400dc847fc6b9dcbc86fcc1059439
GKS <gopakumar.in@gmail.com>
2019-02-03 19:28:02 -0800

    sorted array inetersection optimized solution

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -5,13 +5,33 @@ def intersect_two_sorted_arrays(A, B):
     # TODO - you fill in here.
     #print(B)
     #print(A)
-    A = set(A)
-    A = sorted(A)
+    #A = set(A)
+    #A = sorted(A)
+    #result = list()
+    #for a in A:
+    #    if a in B:
+    #        result.append(a)
+    # 
+    #return result
+    a =0;
+    b=0
     result = list()
-    for a in A:
-        if a in B:
-            result.append(a)
-    
+    last = None
+    while (a < len(A) and b <len(B)):
+        #print (A[a])
+        #print (B[b])
+        if (A[a] == B[b]):
+            if (last != A[a]):
+                result.append(A[a])
+                last = A[a]
+            a = a+1
+            b = b+1
+        elif (A[a] < B[b]):
+            a = a+1
+        else:
+            b= b+1
+        
+    #print (result)
     return result
 
 
