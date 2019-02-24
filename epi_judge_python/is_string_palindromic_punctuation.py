baea856b069fad2ef150884f9c6ad40e530f3952
GKS <gopakumar.in@gmail.com>
2019-02-23 17:15:38 -0800

    string palindrom check

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -3,6 +3,25 @@ from test_framework import generic_test
 
 def is_palindrome(s):
     # TODO - you fill in here.
+    l =  len(s)
+    s = s.lower()
+
+    #print(s)
+    si = 0
+    ei = len(s)-1
+    if(len(s) == 0):
+        return True
+    while (si <= ei):
+        if (s[si].isalnum() == False):
+            si= si+1
+        elif(s[ei].isalnum() == False):
+            ei= ei-1
+        elif(s[si] != s[ei]):
+            return False
+        else:
+            si= si+1
+            ei= ei-1
+            
     return True
 
 
