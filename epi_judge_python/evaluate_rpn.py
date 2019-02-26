d23d740288da7605c5d015a852140d1ec786e513
GKS <gopakumar.in@gmail.com>
2019-02-25 23:27:33 -0800

    posfix evaluation

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -3,7 +3,31 @@ from test_framework import generic_test
 
 def evaluate(expression):
     # TODO - you fill in here.
-    return 0
+        #print(type(expression))
+        
+        listexpr  = expression.split(",")
+        #print (listexpr)
+        #print(int(listexpr[3]))
+        stack = list()
+
+        for i, item in enumerate(listexpr):
+
+            if (len(item)>1):
+                stack.append(int(item))
+            elif(item.isnumeric()):
+                stack.append(int(item))
+            else:
+                op2 = stack.pop()
+                op1 = stack.pop()
+                if(item == '+'):
+                    stack.append(op1+op2)
+                elif(item == '-'):
+                    stack.append(op1-op2)
+                elif(item == '*' or item == 'x' or item == 'X'):
+                    stack.append(op1*op2)
+                elif(item == '/'):
+                    stack.append(int(op1/op2))
+        return stack.pop()
 
 
 if __name__ == '__main__':
