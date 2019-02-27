6e7daac24fab919e44f12833443681ab8c08a460
GKS <gopakumar.in@gmail.com>
2019-02-26 23:24:57 -0800

    balanced Tree using class

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,9 +1,28 @@
 from test_framework import generic_test
+from binary_tree_node import BinaryTreeNode
 
+class solution():
+    def __init__(self):
+        self.result = True
+
+    def inorder(self,node):
+        if(node):
+            #print(node.data)
+            hleft = self.inorder(node.left)
+            hright = self.inorder(node.right)
+            if (abs(hleft-hright) > 1):
+                self.result = False
+                return 0
+            else:
+                return max(hleft,hright)+1
+        else:
+            return 0 
 
 def is_balanced_binary_tree(tree):
-    # TODO - you fill in here.
-    return True
+    # TODO - you fill in here
+    s = solution()
+    s.inorder(tree)
+    return s.result
 
 
 if __name__ == '__main__':
