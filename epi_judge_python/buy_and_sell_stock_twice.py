8a414965df5bcfa82649c12003e169a557e6e72a
GKS <gopakumar.in@gmail.com>
2019-03-18 20:36:03 -0700

    completed string reverse

--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -1,8 +1,44 @@
 from test_framework import generic_test
+import pdb
+#incompleted
+
+class solution ():
+    def __init__(self):
+        self.result = None
+
+    def solution (self,prices):
+        #print (prices)
+        minval = prices[0]
+        maxval = prices[0]
+        profit = list()
+        #pdb.set_trace()
+        pindex = 0
+        profit.append(prices[0] -minval)
+        tempprofit = 0
+        for x in prices:
+            #        print (x,minval,maxval)
+            if x < minval:
+                minval = x
+                profit.append(0)
+                tempprofit = 0
+            if x> minval:
+                #print (profit)
+                #profit[-1] = max(profit[-1],(x-minval))
+                if ((x-minval) > profit[-1] ):
+                    print(x,minval)
+                    profit[-1] = x-minval
+
+        #print (profit)
+        return 0.0
+
+     
+
 
 
 def buy_and_sell_stock_twice(prices):
     # TODO - you fill in here.
+    s = solution()
+    s.solution(prices)
     return 0.0
 
 
--- epi_judge_python/reverse_words.py
+++ epi_judge_python/reverse_words.py
@@ -4,10 +4,46 @@ from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
+
+class solution ():
+    def __init__(self):
+        self.result = bytearray()
+    def solution(self,s):
+        self.reversestring(s,0,len(s))
+
+        
+        start =0
+        for i,val in enumerate(s):
+            if(chr(s[i]) == " "):
+                self.reversestring(s,start,i)
+                start = i+1
+        self.reversestring(s,start,len(s))
+                
+        #self.prints(s)
+        
+    def prints(self,s):
+        for i in range (len(s)):
+            print(i,s[i],chr(s[i]))
+        
+        
+    def reversestring (self,s,start,end):
+        #print(start,end)
+        slen = len(s)
+        for i in range(int((end-start)/2)):
+           # print(i,s[i],chr(s[i]))
+            index = i+start
+           # print(index)
+            s[index],s[end-i-1] = s[end-i-1],s[index]
+        #self.prints(s)
+
+        
+
 # Assume s is a string encoded as bytearray.
 def reverse_words(s):
     # TODO - you fill in here.
-    return
+    s1 = solution()
+    return s1.solution(s)
+    
 
 
 @enable_executor_hook
