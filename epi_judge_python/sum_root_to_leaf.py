c5eadadce6105fcc426688f6f38b77a808ffa93f
GKS <gopakumar.in@gmail.com>
2019-03-20 12:42:15 -0700

    sum root to leaf

--- epi_judge_python/sum_root_to_leaf.py
+++ epi_judge_python/sum_root_to_leaf.py
@@ -1,9 +1,30 @@
 from test_framework import generic_test
 
+class solution ():
+    def __init__ (self):
+        self.result = 0
 
+    def helper(self,node,val):
+        val = val*2+node.data
+        if (node.left == None and node.right == None):
+            self.result = self.result + val
+        else:
+            
+            if(node.left):
+                self.helper(node.left,val)
+            if(node.right):
+                self.helper(node.right,val)
+            
+    
+        
+        
 def sum_root_to_leaf(tree, partial_path_sum=0):
     # TODO - you fill in here.
-    return 0
+
+    r = solution ()
+    if(tree):
+        r.helper(tree,0)
+    return r.result
 
 
 if __name__ == '__main__':
