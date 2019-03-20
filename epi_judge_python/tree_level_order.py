6615b069aa8a424afd20ce74267a47c5b518b018
GKS <gopakumar.in@gmail.com>
2019-03-19 19:01:56 -0700

    tree depth first search

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -1,9 +1,51 @@
 from test_framework import generic_test
+from collections import deque
+class solution():
+    def __init__ (self):
+        self.result = list()
+    def helper(self,q):
+        tempval = []
 
+        while (len(q)>0):
+            for x in range(len(q)):
+                node = q.popleft()
+                tempval.append(node.data)
+                if(node.left):
+                    q.append(node.left)
+                if(node.right):
+                    q.append(node.right)
+            self.result.append(tempval)
+            tempval = []
+        #print(self.result)
+           
+        
 
+    def inorder(self,tree):
+        if(tree == None):
+            return
+        else:
+            self.inorder(tree.left)
+            #print(tree.data)
+            self.inorder(tree.right)
+    def getval(self,A):
+        temp = []
+        for x in range(len(A)):
+            if(A[i] != None):
+                temp.append(A[i].data)
+        return temp
+                
+        
 def binary_tree_depth_order(tree):
     # TODO - you fill in here.
-    return []
+    r = solution()
+    q = deque()
+    if(tree == None):
+        return []
+    q.append(tree)
+    r.helper(q)
+    #r.inorder(tree)
+
+    return r.result
 
 
 if __name__ == '__main__':
