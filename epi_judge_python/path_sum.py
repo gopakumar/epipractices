84b8263c5706599ce4d25eccd62c368fbbd5f556
GKS <gopakumar.in@gmail.com>
2019-03-20 13:03:36 -0700

    path_sum completed

--- epi_judge_python/path_sum.py
+++ epi_judge_python/path_sum.py
@@ -1,9 +1,28 @@
 from test_framework import generic_test
 
-
-def has_path_sum(tree, remaining_weight):
+class solution():
+    def __init__(self):
+        self.result = False
+        self.sum = 0
+    def helper(self,node,val):
+        val = val+node.data
+        if(node.left == None and node.right == None):
+            if (val== self.sum):
+                self.result = True
+        else:
+            if(node.left):
+                self.helper(node.left,val)
+            if(node.right):
+                self.helper(node.right,val)
+        
+        
+def has_path_sum(tree,remaining_weight):
     # TODO - you fill in here.
-    return True
+    r = solution()
+    r.sum = remaining_weight
+    if(tree):
+        r.helper(tree,0)
+    return r.result
 
 
 if __name__ == '__main__':
