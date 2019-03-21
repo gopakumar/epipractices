fcffdc3edf6a588c615b89ce35f704e32589d1d2
GKS <gopakumar.in@gmail.com>
2019-03-20 17:28:21 -0700

    Kth node with out considering the size

--- epi_judge_python/kth_node_in_tree.py
+++ epi_judge_python/kth_node_in_tree.py
@@ -1,10 +1,26 @@
 import functools
-
+import pdb
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
-
+class solution():
+    def __init__(self):
+        self.result= None
+        self.count = 0
+    def helper(self,node,k):
+        #pdb.set_trace()
+        if(node):
+            self.helper(node.left,k)
+            self.count = self.count+1
+            if(self.count == k):
+                self.result = node
+            self.helper(node.right,k)
+        
+            
+                
+            
+    
 class BinaryTreeNode:
     def __init__(self, data=None, left=None, right=None, size=None):
         self.data = data
@@ -14,8 +30,11 @@ class BinaryTreeNode:
 
 
 def find_kth_node_binary_tree(tree, k):
+    
     # TODO - you fill in here.
-    return None
+    r = solution()
+    r.helper(tree,k)
+    return r.result
 
 
 @enable_executor_hook
