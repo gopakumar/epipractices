ef32860b7e1432d0ffc46ddc2c0479d90c6fec24
GKS <gopakumar.in@gmail.com>
2019-03-21 14:47:36 -0700

    added the code changes to check the string is permutable palindrome

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,11 +1,30 @@
 from test_framework import generic_test
 
+from collections import Counter
 
+
+
+class solution():
+    def __init__(self):
+        self.result = True
+    def helper(self,s):
+        oddflag = False
+        c = Counter(s)
+        for k,v in c.items():
+            if (c[k] %2 == 1):
+                if(oddflag ==  True):
+                    self.result = False
+                else:
+                    oddflag = True
+            
+        #print(list(c.elements()))
+        
+        
 def can_form_palindrome(s):
     # TODO - you fill in here.
-    return True
-
-
+    r = solution()
+    r.helper(s)
+    return r.result
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main(
